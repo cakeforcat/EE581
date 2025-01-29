019095c0080a5ba46916c2496232c86f339f69f2
julianstaffelbach <julian.staffelbach@gmail.com>
2025-01-29 21:13:06 +0000

    Median filtering of whole dataset
    9x9 median filter is applied to all dataset, filter size can still be changed but 9x9 seemed like a reasonable size for the strat.

--- src/pre_processing/median_filtering.m
+++ src/pre_processing/median_filtering.m
@@ -2,9 +2,11 @@ clear all;
 close all;
 clc;
 
+% applies 9x9 median filter on all images in dataset. takes roughly 30 mins to execute. 
+
 % work with relatvie paths to data
-originalDataset = '../../data/original_dataset/';
-medianFiltDataset = '../../data/median_filtered';
+originalDataset =   '..\..\data\original_dataset\';
+medianFiltDataset = '..\..\data\median_filtered\';
 
 subfolders_all = dir(fullfile(originalDataset));
 subfolders = {};
@@ -19,9 +21,26 @@ for i=1:length(subfolders_all)
     subfolders{end+1} = subfolders_all(i).name;
 end
 
+% iterate over every subfolder of dataset
 for i=1:length(subfolders)
-
     images = dir(fullfile(originalDataset, subfolders{i}, 'img', '*.tif'));
 
+    % create output directory
+    filtered_im_location = strcat(medianFiltDataset, subfolders{i}, '\img\');
+    if ~exist(filtered_im_location, 'dir')
+        mkdir(filtered_im_location);            
+    end
+
+    % iterate over every image in each subfolder and apply median filter
+    for j=1:length(images)
+        image_path = strcat(images(j).folder, '\', images(j).name);
+        im = imread(image_path);
+        
+        filt_im(:,:,1) = medfilt2(im(:,:,1), [9, 9]);
+        filt_im(:,:,2) = medfilt2(im(:,:,2), [9, 9]);
+        filt_im(:,:,3) = medfilt2(im(:,:,3), [9, 9]);
+
+        imwrite(filt_im, strcat(filtered_im_location, images(j).name));
+    end
 end
 
