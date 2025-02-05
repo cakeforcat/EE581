8754edfced0b8722685f81af1d40426e624e06a8
julianstaffelbach <julian.staffelbach@gmail.com>
2025-02-05 14:19:49 +0000

    Script for removing white areas in image
    Some of the images have a white area at their border, replace this area with a black area instead. This is done with hough line detection.

--- src/pre_processing/grayscale_histogram.m
+++ src/pre_processing/grayscale_histogram.m
@@ -2,8 +2,6 @@ clear all;
 close all;
 clc;
 
-% applies 9x9 median filter on all images in dataset. takes roughly 30 mins to execute. 
-
 % work with relatvie paths to data
 originalDataset =   '..\..\data\original_dataset\';
 medianFiltDataset = '..\..\data\grayscale_histogram_classified\';
@@ -22,7 +20,8 @@ for i=1:length(subfolders_all)
 end
 
 % iterate over every subfolder of dataset
-for i=1:length(subfolders)
+% for i=1:length(subfolders)
+for i=2:3    
     images = dir(fullfile(originalDataset, subfolders{i}, 'img', '*.tif'));
 
     % create output directory
@@ -38,15 +37,119 @@ for i=1:length(subfolders)
 
         im_gray = rgb2gray(im);
 
-        figure(1);
-        subplot(1,2,1);
-        imshow(im_gray);
+        im_gray_outp = remove_white_areas(im_gray);
 
-        subplot(1,2,2);
-        histogram(im_gray);
+        % figure(1);
+        % subplot(1,3,1);
+        % imshow(im_gray);
+        % 
+        % subplot(1,3,2);
+        % histogram(im_gray);
+        % 
+        % im_gray_outp = remove_white_areas(im_gray);
+        % subplot(1,3,3);
+        % histogram(im_gray_outp);
 
     end
 
+
+end
+
+function image_white_areas_removed = remove_white_areas(original_image)
+
+    % output is original image for now
+    image_white_areas_removed = original_image;
+
+    % detect lines and obtain them
+    edges = edge(original_image, 'canny');
+    [H, theta, rho] = hough(edges);
+    peaks = houghpeaks(H,30);   % get 30 most promenent peaks in hough space
+    if ~isempty(peaks)
+        lines = houghlines(edges, theta, rho, peaks, 'FillGap', 200, 'MinLength', 10);
+    end
+
+
+    figure(1);
+    subplot(1,4,1);
+    imshow(edges);  % plot detected edges
+
+    subplot(1,4,2); % plot hough transform
+    imshow(imadjust(rescale(H)),[],...
+       'XData',theta,...
+       'YData',rho,...
+       'InitialMagnification','fit');
+    xlabel('\theta (degrees)')
+    ylabel('\rho')
+    colormap(gca,hot)
+    hold on;
+    x = theta(peaks(:,2));
+    y = rho(peaks(:,1));
+    plot(x,y,'s','color','green', 'LineWidth',2);
+    axis on;
+    axis normal;
+    hold off;
+
+    subplot(1,4,3);
+    imshow(original_image); % plot original image
+    hold on;
+
+    % for each line, see if it separated the useful image and white space,
+    % remove white space if it does, iterate over every line detected
+    for i=1:length(lines)
+        xy = [lines(i).point1; lines(i).point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',1,'Color','red');  % plot lines over original image
+
+        % get a polynom description of the line based on its start and end points in the form y = ax + m
+        polynom_description = polyfit( [xy(1,1), xy(2,1)], [xy(1,2), xy(2,2)], 1);
+  
+        x_fit = xy(1,1) : xy(2,1); % create x vector of points on line
+        if length(x_fit) < 5    % if line is very short, ignore it
+            continue;
+        end
+     
+        y_fit = round(polyval(polynom_description, x_fit)); % create y vector of points on line
+
+        % check if either the area above or below the line is completely
+        % white, over all of line and increase counter
+        white_area_detected = 0;
+        for j=1:length(x_fit)
+            res1 = all(original_image(1:(y_fit(j) - 1), x_fit(j)) == 255, 'all')
+            res2 = all(original_image((y_fit(j) + 1):512, x_fit(j)) == 255, 'all')
+            if ( res1 | res2 )
+                white_area_detected = white_area_detected + 1;
+            end
+        end
+        
+        % if area above or below line is not white over the whole line,
+        % continue with next line in list
+        if white_area_detected < (length(x_fit) - 1)
+            pause(0.05);
+            continue;
+        end
+
+        % do another line fit, this time extend the line to the edges of
+        % the image
+        x_fit = 1:512;  % x line points stretches over whole image
+        y_fit = round(polyval(polynom_description, x_fit));
+
+        % only use points that are within the image
+        valid_y_idx = y_fit >= 1 & y_fit <= 512;
+        
+        % over the whole image, colour the white areas black
+        for j=1:512
+            if valid_y_idx(j)
+                if all(original_image(1:y_fit(j) - 1, j) == 255, 'all')
+                    image_white_areas_removed(1:y_fit(j), j) = 0;
+                elseif all(original_image((y_fit(j) + 1):512, j) == 255, 'all')
+                    image_white_areas_removed((y_fit(j) + 1):512, j) = 0;
+                end
+            end
+        end
+        break;
+    end
+    
+    subplot(1,4,4); % show output image
+    imshow(image_white_areas_removed);
 
 end
 
