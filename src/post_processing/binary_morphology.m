908671a4ea8321592c6dda4ed358e55caeb74162
julianstaffelbach <julian.staffelbach@gmail.com>
2025-03-19 21:45:52 +0000

    Add script for post processing morphology on predictions. Does not improve f1 score

---
+++ src/post_processing/binary_morphology.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+clc;
+
+
+% work with relatvie paths to data
+originalMasks =   '..\..\data\model_output\predictions\';
+processedMasks = '..\..\data\model_output\predictions_morphology\';
+
+masks_files = dir(fullfile(originalMasks, '*.jpg'));
+
+if ~exist(processedMasks, 'dir')
+    mkdir(processedMasks);            
+end
+
+for i=1:length(masks_files)
+
+    masks_path = fullfile(originalMasks, masks_files(i).name);
+    mask = imread(masks_path);
+    mask = mask > 0;
+
+    se = strel('disk', 3);
+    im_morphology = imclose(mask, se);
+
+    imwrite(im_morphology, strcat(processedMasks, masks_files(i).name));
+end
