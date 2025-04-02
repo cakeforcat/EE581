91c3e3d4d4d946b345c1d9e84a4694ffde532d92
Jakub <jakub.czarny.2020@uni.strath.ac.uk>
2025-04-02 14:42:27 +0100

    fix up histograms + batching

--- src/histograms/histograms.py
+++ src/histograms/histograms.py
@@ -14,7 +14,12 @@ if __name__ == '__main__':
             print(f'{i}: {folder.name}')
             sets_idx.append(i)
 
-    for data_set in sets_idx:
+    batch_size = 500
+    nbins = 100
+    bin_edges = np.linspace(0, 255, nbins + 1)
+    hist_table_total_super = np.zeros((3, nbins))
+    hist_table_masked_total_super = np.zeros((3, nbins))
+    for data_set in [15, 16]:
         # load data set
         data_set_path = data_path / list(data_path.glob('*'))[int(data_set)]
         # check if images and labels have the same length
@@ -29,13 +34,10 @@ if __name__ == '__main__':
 
 
         # load the images and labels in batches
-        batch_size = 500
-        nbins = 100
         hist_table_total = np.zeros((3, nbins))
         hist_table_masked_total = np.zeros((3, nbins))
-        print(f'Selected {data_set_path.name}, reading images and labels...')
         total_batch_len = ceil(len(img_label_pairs)/batch_size)
-        bin_edges = np.linspace(0, 255, nbins + 1)
+        print(f'Selected {data_set_path.name}, reading images and labels...')
         for batch_no, batch in enumerate(itertools.batched(img_label_pairs, batch_size)):
             images = []
             labels = []
@@ -72,9 +74,9 @@ if __name__ == '__main__':
             hist_table = np.zeros((3, nbins))
             hist_table_masked = np.zeros((3, nbins))
             for i in range(3):
-                hist_table[i], _ = np.histogram(feature_vector_table[i], bins=bin_edges)
+                hist_table[i], _ = np.histogram(feature_vector_table[i], bins=nbins, range=(1, 254))
                 hist_table_total[i] += hist_table[i]
-                hist_table_masked[i], _ = np.histogram(feature_vector_table_masked[i], bins=bin_edges)
+                hist_table_masked[i], _ = np.histogram(feature_vector_table_masked[i], bins=nbins, range=(1, 254))
                 hist_table_masked_total[i] += hist_table_masked[i]
 
             print(f'Processed images and labels in batch: {batch_no+1} out of {total_batch_len}!')
@@ -88,3 +90,17 @@ if __name__ == '__main__':
             axs[1, i].hist(bin_edges[:-1], bins=bin_edges,weights=hist_table_masked_total[i], color=col)
             axs[1, i].set_title(f'ch: {col} masked')
         plt.savefig(f'histograms_{data_set_path.name}.png')
+
+        for i in range(3):
+            hist_table_total_super[i] += hist_table_total[i]
+            hist_table_masked_total_super[i] += hist_table_masked_total[i]
+
+    # plot super histogram
+    fig, axs = plt.subplots(2, 3)
+    fig.tight_layout(pad=3.0)
+    for i, col in enumerate(('r', 'g', 'b')):
+        axs[0, i].hist(bin_edges[:-1], bins=bin_edges,weights=hist_table_total_super[i], color=col)
+        axs[0, i].set_title(f'ch: {col}')
+        axs[1, i].hist(bin_edges[:-1], bins=bin_edges,weights=hist_table_masked_total_super[i], color=col)
+        axs[1, i].set_title(f'ch: {col} masked')
+    plt.savefig(f'histograms_{data_path.name}.png')
